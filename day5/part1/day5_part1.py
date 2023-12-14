1df197f272992a8e991841a70efc4cd3b7d4c308
AnthonyT <antonis301@gmail.com>
2023-12-15 01:15:08 +0200

    Refactor day 5 part 1

--- day5/part1/day5_part1.py
+++ day5/part1/day5_part1.py
@@ -1,5 +1,4 @@
 import time
-import threading
 inputFile = open('input.txt', 'r')
 data = inputFile.read()
 
@@ -11,50 +10,39 @@ def strMapToIntMap(strMap):
             intMap.append([int(i) for i in l.split(' ')])
     return intMap
 
-seedsStr = data.split('seeds: ')[1].split('seed-to-soil map:\n')[0].strip()
-seedToSoilMapStr = data.split('seed-to-soil map:\n')[1].split('\nsoil-to-fertilizer map:')[0]
-soilToFertilizerMapStr = data.split('soil-to-fertilizer map:\n')[1].split('\nfertilizer-to-water map:')[0]
-fertilizerToWaterMapStr = data.split('fertilizer-to-water map:\n')[1].split('\nwater-to-light map:')[0]
-waterToLightMapStr = data.split('water-to-light map:\n')[1].split('\nlight-to-temperature map:')[0]
-lightToTempMapStr = data.split('light-to-temperature map:\n')[1].split('\ntemperature-to-humidity map:')[0]
-tempToHumidityMapStr = data.split('temperature-to-humidity map:\n')[1].split('\nhumidity-to-location map:')[0]
-humidityToLocationMapStr = data.split('humidity-to-location map:\n')[1]
+strMaps = []
+strMaps.append(data.split('seed-to-soil map:\n')[1].split('\nsoil-to-fertilizer map:')[0])
+strMaps.append(data.split('soil-to-fertilizer map:\n')[1].split('\nfertilizer-to-water map:')[0])
+strMaps.append(data.split('fertilizer-to-water map:\n')[1].split('\nwater-to-light map:')[0])
+strMaps.append(data.split('water-to-light map:\n')[1].split('\nlight-to-temperature map:')[0])
+strMaps.append(data.split('light-to-temperature map:\n')[1].split('\ntemperature-to-humidity map:')[0])
+strMaps.append(data.split('temperature-to-humidity map:\n')[1].split('\nhumidity-to-location map:')[0])
+strMaps.append(data.split('humidity-to-location map:\n')[1])
 
+allMaps = []
+
+for strMap in strMaps:
+    allMaps.append(strMapToIntMap(strMap))
+
+seedsStr = data.split('seeds: ')[1].split('seed-to-soil map:\n')[0].strip()
 seeds = [int(i) for i in seedsStr.split(' ')]
 
-seedToSoilMap = strMapToIntMap(seedToSoilMapStr)
-soilToFertilizerMap = strMapToIntMap(soilToFertilizerMapStr)
-fertilizerToWaterMap = strMapToIntMap(fertilizerToWaterMapStr)
-waterToLightMap = strMapToIntMap(waterToLightMapStr)
-lightToTempMap = strMapToIntMap(lightToTempMapStr)
-tempToHumidityMap = strMapToIntMap(tempToHumidityMapStr)
-humidityToLocationMap = strMapToIntMap(humidityToLocationMapStr)
 
-def mapPoint(mapList, point):
-    for m in mapList:
-        if point >= m[1] and point <= m[1]+m[2]:
-            return point - (m[1]-m[0])
-
-    return point
+def mapPoint(p):
+    for Map in allMaps:
+        for mapRange in Map:
+            if p >= mapRange[1] and p <= mapRange[1] + mapRange[2]:
+                p = p - (mapRange[1] - mapRange[0])
+                break
+    return p
 
 def getLocations():
     locations = []
     for s in seeds:
-        soil = mapPoint(seedToSoilMap, s)
-        print('Seed {0} mapped to {1} soil.'.format(s, soil))
-        fertilizer = mapPoint(soilToFertilizerMap, soil)
-        print('Soil {0} mapped to {1} fertilizer'.format(soil, fertilizer))
-        water = mapPoint(fertilizerToWaterMap, fertilizer)
-        print('Fertilizer {0} mapped to {1} water'.format(fertilizer, water))
-        light = mapPoint(waterToLightMap, water)
-        print('Water {0} mapped to {1} light'.format(water, light))
-        temp = mapPoint(lightToTempMap, light)
-        print('Light {0} mapped to {1} temp'.format(light, temp))
-        humidity = mapPoint(tempToHumidityMap, temp)
-        print('Temp {0} mapped to {1} humidity'.format(temp, humidity))
-        location = mapPoint(humidityToLocationMap, humidity)
-        print('Humidity {0} mapped to {1} location'.format(humidity, location))
-        locations.append(location)
+        mapped = mapPoint(s)
+        print('Point {0} mapped to {1}'.format(s, mapped))
+        locations.append(mapped)
+
     return locations
     
 
