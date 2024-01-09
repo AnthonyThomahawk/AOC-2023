2c5900a433f5e614bbb913950add7472dcd35e89
Anthony T <antonis301@gmail.com>
2024-01-09 04:09:14 +0200

    refactor

--- day7/part1/day7_part1.py
+++ day7/part1/day7_part1.py
@@ -1,5 +1,4 @@
 from enum import Enum
-import time
 inputFile = open('day7/part1/input.txt', 'r')
 hands = []
 bids = []
@@ -58,41 +57,6 @@ def getHandType(h):
                         if h.count(x) == 2:
                             return handTypes['TwoPair']
                     return handTypes['OnePair']
-
-
-# A similar solution that did NOT ALWAYS work, some times it returned None?!
-
-# def getHandType(h):
-#     if h.count(h[0]) == 5:
-#         return handTypes['FiveOfAKind']
-#     else:
-#         s = set(h)
-#         if len(s) == len(h):
-#             return handTypes['HighCard']
-#         foundThree = False
-#         foundTwo = False
-#         for c in s:
-#             if foundThree:
-#                 if h.count(c) == 2:
-#                     return handTypes['FullHouse']
-#                 else:
-#                     return handTypes['ThreeOfAKind']
-#             if foundTwo:
-#                 if h.count(c) == 3:
-#                     return handTypes['FullHouse']
-#                 elif h.count(c) == 2:
-#                     return handTypes['TwoPair']
-#                 else:
-#                     return handTypes['OnePair']
-
-#             if h.count(c) == 4:
-#                 return handTypes['FourOfAKind']
-#             elif h.count(c) == 3:
-#                 foundThree = True
-#                 continue
-#             elif h.count(c) == 2:
-#                 foundTwo = True
-#                 continue
 
 def compareHands(h1, h2):
     t1 = getHandType(h1)
